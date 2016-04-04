148f4b35b540f40a0359009dda9e6c5765a7b753
eugenp <hanriseldon@gmail.com>
2016-04-04 11:43:26 +0300

    initial module1 codebase

---
+++ m1-lesson2/src/main/java/com/baeldung/lss/web/controller/UserController.java
@@ -0,0 +1,73 @@
+package com.baeldung.lss.web.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.baeldung.lss.persistence.UserRepository;
+import com.baeldung.lss.web.model.User;
+
+@Controller
+@RequestMapping("/")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    //
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @RequestMapping
+    public ModelAndView list() {
+        Iterable<User> users = this.userRepository.findAll();
+        return new ModelAndView("users/list", "users", users);
+    }
+
+    @RequestMapping("{id}")
+    public ModelAndView view(@PathVariable("id") User user) {
+        return new ModelAndView("users/view", "user", user);
+    }
+
+    @RequestMapping(params = "form", method = RequestMethod.GET)
+    public String createForm(@ModelAttribute User user) {
+        return "users/form";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ModelAndView create(@Valid User user, BindingResult result, RedirectAttributes redirect) {
+        if (result.hasErrors()) {
+            return new ModelAndView("users/form", "formErrors", result.getAllErrors());
+        }
+        user = this.userRepository.save(user);
+        redirect.addFlashAttribute("globalMessage", "Successfully created a new user");
+        return new ModelAndView("redirect:/{user.id}", "user.id", user.getId());
+    }
+
+    @RequestMapping("foo")
+    public String foo() {
+        throw new RuntimeException("Expected exception in controller");
+    }
+
+    @RequestMapping(value = "delete/{id}")
+    public ModelAndView delete(@PathVariable("id") Long id) {
+        this.userRepository.deleteUser(id);
+        return new ModelAndView("redirect:/");
+    }
+
+    @RequestMapping(value = "modify/{id}", method = RequestMethod.GET)
+    public ModelAndView modifyForm(@PathVariable("id") User user) {
+        return new ModelAndView("users/form", "user", user);
+    }
+
+}
---
+++ m1-lesson4/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.baeldung.lss.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    public LssSecurityConfig() {
+        super();
+    }
+
+    //
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception { // @formatter:off 
+        auth.
+            inMemoryAuthentication().
+            withUser("user").password("pass").
+            roles("USER");
+    } // @formatter:on
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception { // @formatter:off
+        http
+        .authorizeRequests()
+                .antMatchers("/delete/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+        
+        .and()
+        .formLogin()
+        ;
+    } // @formatter:on
+
+}
