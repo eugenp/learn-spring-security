43f813b8563fcfa72d1b68483f7445f85432c4d4
Eugen Paraschiv <eugen@baeldung.com>
2016-05-15 12:44:09 -0500

    initial work on module 8

---
+++ m8-lesson1/src/main/java/com/baeldung/lss/validation/ValidPassword.java
@@ -0,0 +1,26 @@
+package com.baeldung.lss.validation;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Documented
+@Constraint(validatedBy = PasswordConstraintValidator.class)
+@Target({ TYPE, FIELD, ANNOTATION_TYPE })
+@Retention(RUNTIME)
+public @interface ValidPassword {
+
+    String message() default "Invalid Password";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
---
+++ m8-lesson4/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -0,0 +1,62 @@
+package com.baeldung.lss.spring;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    public LssSecurityConfig() {
+        super();
+    }
+
+    //
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {// @formatter:off
+        //        auth.inMemoryAuthentication().
+        //            withUser("test@email.com").password("pass").roles("USER").and().
+        //            withUser("test2@email.com").password("pass2").roles("ADMIN");
+        
+        auth.
+            jdbcAuthentication().dataSource(dataSource). // withDefaultSchema().
+            withUser("test@email.com").password("pass").roles("USER");
+    }// @formatter:on
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception { // @formatter:off
+        http
+        .authorizeRequests()
+                .antMatchers("/signup",
+                        "/user/register",
+                        "/registrationConfirm*",
+                        "/badUser*",
+                        "/forgotPassword*",
+                        "/user/resetPassword*",
+                        "/user/changePassword*",
+                        "/user/savePassword*",
+                        "/js/**").permitAll()
+                .anyRequest().authenticated()
+
+        .and()
+        .formLogin().
+            loginPage("/login").permitAll().
+            loginProcessingUrl("/doLogin")
+
+        .and()
+        .logout().permitAll().logoutUrl("/logout")
+
+        .and()
+        .csrf().disable()
+        ;
+    } // @formatter:on
+
+}
