257f952dda6472fab1e4bd8a437f0cdc633fda8a
Eugen Paraschiv <eugen@baeldung.com>
2016-07-21 04:47:47 -0500

    starting point for lesson 3

--- m15-lesson3/src/main/java/com/baeldung/lss/spring/LssHttpSessionConfig.java
+++ m15-lesson3/src/main/java/com/baeldung/lss/spring/LssHttpSessionConfig.java
@@ -1,7 +1,9 @@
 package com.baeldung.lss.spring;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@Configuration
 @EnableRedisHttpSession
 public class LssHttpSessionConfig {
 
