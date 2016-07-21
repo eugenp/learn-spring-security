78ceb3f226a26562b6a58c4f7d50817c1407dbf1
Eugen Paraschiv <eugen@baeldung.com>
2016-07-21 05:10:21 -0500

    changing order of lessons

--- m13-lesson3-start/m13-lesson3-server/src/main/java/com/baeldung/um/validation/EmailExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.um.validation;
-
-public class EmailExistsException extends Throwable {
-
-    public EmailExistsException(final String message) {
-        super(message);
-    }
-
-}
