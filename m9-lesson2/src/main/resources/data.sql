7cf9a4a52c3f4877e68983774ffbfb7c841a6fc4
Eugen Paraschiv <eugen@baeldung.com>
2017-02-11 12:39:59 -0600

    new module and cleanup work

---
+++ m9-lesson2/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into Privilege (name, id) values ('READ_PRIVILEGE', 1);
+insert into Privilege (name, id) values ('WRITE_PRIVILEGE', 2);
+insert into Role (name, id) values ('ROLE_ADMIN', 3);
+insert into Role (name, id) values ('ROLE_USER', 4);
+insert into roles_privileges (role_id, privilege_id) values (3, 1);
+insert into roles_privileges (role_id, privilege_id) values (3, 2);
+insert into roles_privileges (role_id, privilege_id) values (4, 1);
+insert into User (id, created, email, password) values (1, NOW(), 'test@test.com', 'test');
+insert into users_roles (user_id, role_id) values (1, 3);
