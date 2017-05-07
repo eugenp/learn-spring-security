1de7945251a67ffdfd32639f71ee4e5ecaeb1063
Eugen Paraschiv <eugen@baeldung.com>
2017-05-07 03:36:57 -0500

    maven upgrades, cleanup work

--- m1-lesson7/src/main/java/com/baeldung/lss/spring/LssApp7.java
+++ m1-lesson7/src/main/java/com/baeldung/lss/spring/LssApp7.java
@@ -1,14 +1,9 @@
 package com.baeldung.lss.spring;
 
-import com.baeldung.lss.persistence.UserRepository;
-import com.baeldung.lss.web.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -17,21 +12,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan("com.baeldung.lss.web.model")
 public class LssApp7 {
 
-    @Autowired
-    private UserRepository userRepository;
-
-    @Bean
-    public Converter<String, User> messageConverter() {
-        return new Converter<String, User>() {
-            @Override
-            public User convert(String id) {
-                return userRepository.findOne(Long.valueOf(id));
-            }
-        };
-    }
-
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(new Class[]{LssApp7.class, LssSecurityConfig.class, LssWebMvcConfiguration.class}, args);
+        SpringApplication.run(new Class[] { LssApp7.class, LssSecurityConfig.class, LssWebMvcConfiguration.class }, args);
     }
 
 }
