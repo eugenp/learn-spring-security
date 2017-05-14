ecb163764bce5bb86e3a860d6dfddbb4e7e70078
Eugen Paraschiv <eugen@baeldung.com>
2017-05-14 05:40:51 -0500

    new module and cleanup work

--- m9-lesson2/src/main/resources/data.sql
+++ m9-lesson2/src/main/resources/data.sql
@@ -5,5 +5,5 @@ insert into Role (name, id) values ('ROLE_USER', 4);
 insert into roles_privileges (role_id, privilege_id) values (3, 1);
 insert into roles_privileges (role_id, privilege_id) values (3, 2);
 insert into roles_privileges (role_id, privilege_id) values (4, 1);
-insert into User (id, created, email, password) values (1, NOW(), 'test@test.com', 'test');
+insert into User (id, created, email, password) values (1, NOW(), 'test@email.com', 'pass');
 insert into users_roles (user_id, role_id) values (1, 3);
--- m9-lesson4/src/main/java/com/baeldung/lss/spring/LssApp4.java
+++ m9-lesson4/src/main/java/com/baeldung/lss/spring/LssApp4.java
@@ -1,34 +1,17 @@
 package com.baeldung.lss.spring;
 
-import com.baeldung.lss.persistence.UserRepository;
-import com.baeldung.lss.web.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan("com.baeldung.lss.web")
+@ComponentScan({"com.baeldung.lss.web","com.baeldung.lss.spring"})
 @EnableJpaRepositories("com.baeldung.lss")
 @EntityScan("com.baeldung.lss.web.model")
-public class LssApp4 {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Bean
-    public Converter<String, User> messageConverter() {
-        return new Converter<String, User>() {
-            @Override
-            public User convert(String id) {
-                return userRepository.findOne(Long.valueOf(id));
-            }
-        };
-    }
+public class LssApp4 extends SpringBootServletInitializer{
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(new Class[] { LssApp4.class, LssSecurityConfig.class, LssWebMvcConfiguration.class }, args);
