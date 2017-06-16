e8ccecfabb91d8e439713fe67c4109649057a62a
Eugen Paraschiv <eugen@baeldung.com>
2017-06-16 15:55:10 -0500

    small cleanup work

--- m1-lesson6/src/main/java/com/baeldung/lss/spring/LssApp6.java
+++ m1-lesson6/src/main/java/com/baeldung/lss/spring/LssApp6.java
@@ -2,32 +2,11 @@ package com.baeldung.lss.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.convert.converter.Converter;
-
-import com.baeldung.lss.persistence.InMemoryUserRepository;
-import com.baeldung.lss.persistence.UserRepository;
-import com.baeldung.lss.web.model.User;
 
 @SpringBootApplication
 @ComponentScan("com.baeldung.lss.web")
-public class LssApp6 {
-
-    @Bean
-    public UserRepository userRepository() {
-        return new InMemoryUserRepository();
-    }
-
-    @Bean
-    public Converter<String, User> messageConverter() {
-        return new Converter<String, User>() {
-            @Override
-            public User convert(String id) {
-                return userRepository().findUser(Long.valueOf(id));
-            }
-        };
-    }
+public class LssApp6 {    
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(new Class[] { LssApp6.class, LssSecurityConfig.class, LssWebMvcConfiguration.class }, args);
--- m1-lesson6/src/main/java/com/baeldung/lss/spring/LssWebMvcConfiguration.java
+++ m1-lesson6/src/main/java/com/baeldung/lss/spring/LssWebMvcConfiguration.java
@@ -1,11 +1,20 @@
 package com.baeldung.lss.spring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import com.baeldung.lss.persistence.InMemoryUserRepository;
+import com.baeldung.lss.persistence.UserRepository;
+import com.baeldung.lss.web.model.User;
+
 @EnableWebMvc
+@Configuration
 public class LssWebMvcConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
@@ -13,6 +22,22 @@ public class LssWebMvcConfiguration extends WebMvcConfigurerAdapter {
         registry.addViewController("/login").setViewName("loginPage");
 
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+    
+    @Bean
+    public UserRepository userRepository() {
+        return new InMemoryUserRepository();
+    }
+    
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new  Converter<String, User>() {
+            @Override
+            public User convert(String id) {
+                return userRepository().findUser(Long.valueOf(id));
+            }
+        });
+       
     }
 
 }
