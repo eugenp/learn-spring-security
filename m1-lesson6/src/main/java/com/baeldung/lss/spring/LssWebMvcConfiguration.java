c425f6dd799020e037ba9af64dd176327f9bfe3a
Eugen Paraschiv <eugen@baeldung.com>
2017-06-17 09:51:42 -0500

    new module and cleanup work

--- m1-lesson6/src/main/java/com/baeldung/lss/spring/LssWebMvcConfiguration.java
+++ m1-lesson6/src/main/java/com/baeldung/lss/spring/LssWebMvcConfiguration.java
@@ -1,6 +1,6 @@
 package com.baeldung.lss.spring;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.convert.converter.Converter;
@@ -9,32 +9,29 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import com.baeldung.lss.persistence.InMemoryUserRepository;
 import com.baeldung.lss.persistence.UserRepository;
 import com.baeldung.lss.web.model.User;
 
 @EnableWebMvc
 @Configuration
 public class LssWebMvcConfiguration extends WebMvcConfigurerAdapter {
+    
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("loginPage");
 
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-    }
-    
-    @Bean
-    public UserRepository userRepository() {
-        return new InMemoryUserRepository();
-    }
+    }       
     
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new  Converter<String, User>() {
             @Override
             public User convert(String id) {
-                return userRepository().findUser(Long.valueOf(id));
+                return userRepository.findUser(Long.valueOf(id));
             }
         });
        
