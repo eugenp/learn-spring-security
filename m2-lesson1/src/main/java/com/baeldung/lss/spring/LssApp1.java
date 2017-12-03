5cf2a3d56a7fdec323239127b49c5d38a8216c68
Eugen <eugen@baeldung.com>
2017-12-03 14:04:47 -0800

    minor lesson upgrades

--- m2-lesson1/src/main/java/com/baeldung/lss/spring/LssApp1.java
+++ m2-lesson1/src/main/java/com/baeldung/lss/spring/LssApp1.java
@@ -3,6 +3,8 @@ package com.baeldung.lss.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,10 +12,21 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan("com.baeldung.lss")
 @EnableJpaRepositories("com.baeldung.lss")
 @EntityScan("com.baeldung.lss.web.model")
-public class LssApp1 {
+public class LssApp1 extends SpringBootServletInitializer {
+	
+	private final static Class<?>[] CONFIGS = { // @formatter:off
+			LssSecurityConfig.class,
+			LssWebMvcConfiguration.class,           
+			LssApp1.class            
+    }; // // @formatter:on
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(new Class[] { LssApp1.class, LssSecurityConfig.class, LssWebMvcConfiguration.class }, args);
-    }
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CONFIGS);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(CONFIGS,args);
+	}
 
 }
