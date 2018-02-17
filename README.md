3a79de98eacc531206c00370881aa9b4ef9995e1
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2018-02-17 08:13:33 +0100

    M14 L2

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Module 14 - Two-Factor Authentication
 This is the codebase for Module 14 of [Learn Spring Security](http://bit.ly/github-lss)
+
+_m14-lesson4_ uses Twilio SDK version 3.x
+
+_m14-lesson4-new_ uses Twilio SDK version 7.x
+
---
+++ m14-lesson3-new/src/main/java/com/baeldung/lss/web/controller/RegistrationController.java
@@ -0,0 +1,43 @@
+package com.baeldung.lss.web.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.baeldung.lss.service.IUserService;
+import com.baeldung.lss.validation.EmailExistsException;
+import com.baeldung.lss.web.model.User;
+
+@Controller
+class RegistrationController {
+
+    @Autowired
+    private IUserService userService;
+
+    //
+
+    @RequestMapping(value = "signup")
+    public ModelAndView registrationForm() {
+        return new ModelAndView("registrationPage", "user", new User());
+    }
+
+    @RequestMapping(value = "user/register")
+    public ModelAndView registerUser(@Valid final User user, final BindingResult result) {
+        if (result.hasErrors()) {
+            return new ModelAndView("registrationPage", "user", user);
+        }
+        try {
+            userService.registerNewUser(user);
+        } catch (EmailExistsException e) {
+            result.addError(new FieldError("user", "email", e.getMessage()));
+            return new ModelAndView("registrationPage", "user", user);
+        }
+        return new ModelAndView("redirect:/login");
+    }
+
+}
