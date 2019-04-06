ec847b87dfc87b443447dc85aa548aaf83dc5aef
Loredana <lore.crusoveanu@gmail.com>
2019-04-06 16:53:00 +0300

    update to Boot 2, clean pom

--- m6-lesson2/src/test/java/com/baeldung/test/Lss2IntegrationTest.java
+++ m6-lesson2/src/test/java/com/baeldung/test/Lss2IntegrationTest.java
@@ -1,9 +1,20 @@
 package com.baeldung.test;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+
+import javax.servlet.Filter;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.baeldung.lss.spring.LssApp2;
 
@@ -11,6 +22,25 @@ import com.baeldung.lss.spring.LssApp2;
 @SpringBootTest(classes= {LssApp2.class})
 public class Lss2IntegrationTest {
 
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private Filter springSecurityFilterChain;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).addFilters(springSecurityFilterChain).build();
+    }
+
+    @Test
+    public void testUserLoginSuccess() throws Exception {
+        ResultActions resultActions = mockMvc.perform(formLogin("/doLogin").user("test@email.com").password("pass"));
+        resultActions.andExpect(authenticated());
+    }
+    
     @Test
     public void whenLoadApplication_thenSuccess() {
 
--- m6-lesson5/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ m6-lesson5/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -4,19 +4,21 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 import com.baeldung.lss.model.User;
 import com.baeldung.lss.persistence.UserRepository;
 
 @EnableWebSecurity
+@Configuration
 public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -74,7 +76,7 @@ public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new StandardPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 
 }
