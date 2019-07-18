a4c22399219bae9793f5cc9811afc4c8d3d40f89
Loredana <lore.crusoveanu@gmail.com>
2019-07-18 10:14:51 +0300

    update webmvc interface

--- m12-lesson1/src/main/java/com/baeldung/um/spring/UmWebMvcConfiguration.java
+++ m12-lesson1/src/main/java/com/baeldung/um/spring/UmWebMvcConfiguration.java
@@ -1,11 +1,12 @@
 package com.baeldung.um.spring;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
+@Configuration
 public class UmWebMvcConfiguration implements WebMvcConfigurer {
-
     //
 
 }
