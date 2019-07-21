ecf3ca669add36416a884f1540579fcb5632a18a
Loredana <lore.crusoveanu@gmail.com>
2019-07-21 21:46:40 +0300

    fix session expiration

--- m8-lesson5/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ m8-lesson5/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -3,6 +3,7 @@ package com.baeldung.lss.spring;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 import com.baeldung.lss.model.User;
 import com.baeldung.lss.persistence.UserRepository;
@@ -70,5 +72,10 @@ public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public SessionRegistry sessionRegistry() {
         return new SessionRegistryImpl();
+    }
+    
+    @Bean
+    public static ServletListenerRegistrationBean httpSessionEventPublisher() {	
+        return new ServletListenerRegistrationBean(new HttpSessionEventPublisher());
     }
 }
