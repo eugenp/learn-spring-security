73cd390824ffa59c857466c43e6437d2f63217b1
Loredana <lore.crusoveanu@gmail.com>
2020-05-09 15:10:26 +0300

    add missing l2 code, boot upgrade

---
+++ m5-on-method-auth-expressions-end/src/main/java/com/baeldung/lss/spring/OnMethodAuthExprEndLssApp.java
@@ -0,0 +1,19 @@
+package com.baeldung.lss.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan("com.baeldung.lss.web")
+@EnableJpaRepositories("com.baeldung.lss")
+@EntityScan("com.baeldung.lss.web.model")
+public class OnMethodAuthExprEndLssApp {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(new Class[] { OnMethodAuthExprEndLssApp.class, LssSecurityConfig.class, LssWebMvcConfiguration.class }, args);
+    }
+
+}
