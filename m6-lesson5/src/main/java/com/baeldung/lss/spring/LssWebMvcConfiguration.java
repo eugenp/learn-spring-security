05cdffb7404f269aea516e2d690ac64535f3fb8f
Loredana <lore.crusoveanu@gmail.com>
2020-05-11 13:21:51 +0300

    fix mvc bug, boot upgrade

--- m6-lesson5/src/main/java/com/baeldung/lss/spring/LssWebMvcConfiguration.java
+++ m6-lesson5/src/main/java/com/baeldung/lss/spring/LssWebMvcConfiguration.java
@@ -2,12 +2,10 @@ package com.baeldung.lss.spring;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
 @Configuration
 public class LssWebMvcConfiguration implements WebMvcConfigurer {
 
