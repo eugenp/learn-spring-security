f70ba5252448e670e648ab51dd83fa7dc16571aa
Loredana <lore.crusoveanu@gmail.com>
2020-05-11 13:33:54 +0300

    fix mvc bug, boot upgrade

--- m12-lesson2/src/main/java/com/baeldung/um/spring/UmWebMvcConfiguration.java
+++ m12-lesson2/src/main/java/com/baeldung/um/spring/UmWebMvcConfiguration.java
@@ -1,10 +1,8 @@
 package com.baeldung.um.spring;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
 @Configuration
 public class UmWebMvcConfiguration implements WebMvcConfigurer {
     //
