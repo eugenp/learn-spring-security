2487f3f874e60afbb5c288bbaa101e848d8335f4
Loredana <lore.crusoveanu@gmail.com>
2020-05-28 11:54:17 +0300

    remove old lesson, fix keycloak app start

--- oauth2-client-thirdparty-authserver-start/oauth2-client-thirdparty-authserver-start--resource-server/src/main/java/com/baeldung/lsso/persistence/repository/IProjectRepository.java
+++
@@ -1,8 +0,0 @@
-package com.baeldung.lsso.persistence.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.baeldung.lsso.persistence.model.Project;
-
-public interface IProjectRepository extends PagingAndSortingRepository<Project, Long> {
-}
