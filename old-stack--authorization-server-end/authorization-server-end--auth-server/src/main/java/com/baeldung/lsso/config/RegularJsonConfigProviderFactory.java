572700604e42518ef53ff70c47dcac84bb3c2713
Loredana <lore.crusoveanu@gmail.com>
2020-05-28 11:51:47 +0300

    remove legacy lessons, fix keycloak app start

--- old-stack--authorization-server-end/authorization-server-end--auth-server/src/main/java/com/baeldung/lsso/config/RegularJsonConfigProviderFactory.java
+++
@@ -1,7 +0,0 @@
-package com.baeldung.lsso.config;
-
-import org.keycloak.services.util.JsonConfigProviderFactory;
-
-public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory {
-
-}
--- old-stack--authorization-server-end/authorization-server-end--resource-server/src/main/java/com/baeldung/um/validation/EmailExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.um.validation;
-
-public class EmailExistsException extends RuntimeException {
-
-    public EmailExistsException(final String message) {
-        super(message);
-    }
-
-}
