b94bee61eb6a6abbbabaca08bd8a92482697c098
Gerardo Roza <rozagerardo@gmail.com>
2020-06-20 13:48:24 -0300

    created new lssc-module3 branch out of LSS for the LSSC course

---
+++ README.md
@@ -0,0 +1,2 @@
+# Learn Spring Security Core - Remember Me
+This is the codebase for Module 'Remember Me' of [Learn Spring Security Core](https://bit.ly/github-lssc)
---
+++ m3-lesson3/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -0,0 +1,83 @@
+package com.baeldung.lss.spring;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+@EnableWebSecurity
+@Configuration
+public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private DataSource dataSource;
+
+    public LssSecurityConfig() {
+        super();
+    }
+
+    //
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception { // @formatter:off
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    } // @formatter:on
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception { // @formatter:off
+        http
+        .authorizeRequests()
+                .antMatchers("/signup",
+                        "/user/register",
+                        "/registrationConfirm*",
+                        "/badUser*",
+                        "/forgotPassword*",
+                        "/user/resetPassword*",
+                        "/user/changePassword*",
+                        "/user/savePassword*",
+                        "/js/**").permitAll()
+                .anyRequest().authenticated()
+
+        .and()
+        .formLogin().
+            loginPage("/login").permitAll().
+            loginProcessingUrl("/doLogin")
+
+        .and()
+        .rememberMe().tokenRepository(persistentTokenRepository())
+            
+
+        .and()
+        .logout().permitAll().logoutUrl("/logout")
+
+        .and()
+        .csrf().disable()
+        ;
+    } // @formatter:on
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        final JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        return jdbcTokenRepository;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
