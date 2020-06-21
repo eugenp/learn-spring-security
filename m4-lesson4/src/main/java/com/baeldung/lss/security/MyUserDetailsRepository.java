3a049a3e7df9ac38e90b120f518270e9f4ba6641
Loredana <lore.crusoveanu@gmail.com>
2020-06-21 15:58:12 +0300

    upgrade to Boot 2

--- m4-lesson4/src/main/java/com/baeldung/lss/security/MyUserDetailsRepository.java
+++ m4-lesson4/src/main/java/com/baeldung/lss/security/MyUserDetailsRepository.java
@@ -3,6 +3,9 @@ package com.baeldung.lss.security;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,12 +15,18 @@ public class MyUserDetailsRepository {
 
     public MyUserDetailsRepository() {
         users = new HashMap<String, MyUserDetails>();
-        users.put("user", new MyUserDetails("user", "pass", "USER", "FirstOrganization"));
-        users.put("john", new MyUserDetails("john", "123", "USER", "SecondOrganization"));
-        users.put("tom", new MyUserDetails("tom", "111", "ADMIN", "SecondOrganization"));
+        users.put("user", new MyUserDetails("user", passwordEncoder().encode("pass"), "USER", "FirstOrganization"));
+        users.put("john", new MyUserDetails("john", passwordEncoder().encode("123"), "USER", "SecondOrganization"));
+        users.put("tom", new MyUserDetails("tom", passwordEncoder().encode("111"), "ADMIN", "SecondOrganization"));
     }
 
     public MyUserDetails findByUsername(String username) {
         return users.get(username);
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
