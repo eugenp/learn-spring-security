21462f88380fe78bf8bd0132c6a5b9b0d5d7032e
Gerardo Roza <rozagerardo@gmail.com>
2020-10-31 12:05:01 -0300

    reverted all changes in lssc-module14 branch to match the video in the lessons

--- m14-lesson2/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ m14-lesson2/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -1,36 +1,23 @@
 package com.baeldung.lss.spring;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
-@Configuration
 public class LssSecurityConfig {
 
+    @SuppressWarnings("deprecation")
     @Bean
     public MapReactiveUserDetailsService userDetailsService() {
-        UserDetails user = User.builder()
-                .passwordEncoder(passwordEncoder()::encode)
-                .username("user")
-                .password("pass")
-                .roles("USER")
-                .build();
-        UserDetails admin = User.builder()
-                .passwordEncoder(passwordEncoder()::encode)
-                .username("admin")
-                .password("pass")
-                .roles("ADMIN")
-                .build();
+        UserDetails user = User.withDefaultPasswordEncoder().username("user").password("pass").roles("USER").build();
+        UserDetails admin = User.withDefaultPasswordEncoder().username("admin").password("pass").roles("ADMIN").build();
 
         return new MapReactiveUserDetailsService(user, admin);
     }
@@ -49,11 +36,6 @@ public class LssSecurityConfig {
             .disable()
                 .build();
          // @formatter:on
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(10);
     }
 
 }
