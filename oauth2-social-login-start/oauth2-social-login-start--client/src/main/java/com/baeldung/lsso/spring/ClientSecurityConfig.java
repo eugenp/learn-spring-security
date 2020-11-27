74f5f8d0efbbe46f2a39f11b98e471e0622acf67
Loredana Crusoveanu <lore.crusoveanu@gmail.com>
2020-11-27 17:31:04 +0200

    upgrade boot version, code cleanup

--- oauth2-social-login-start/oauth2-social-login-start--client/src/main/java/com/baeldung/lsso/spring/ClientSecurityConfig.java
+++ oauth2-social-login-start/oauth2-social-login-start--client/src/main/java/com/baeldung/lsso/spring/ClientSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.baeldung.lsso.spring;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
--- refresh-token/refresh-token--client/src/test/java/com/baeldung/lsso/ContextIntegrationTest.java
+++ refresh-token/refresh-token--client/src/test/java/com/baeldung/lsso/ContextIntegrationTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import com.baeldung.lsso.LssoClientApplication;
-
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = { LssoClientApplication.class })
 public class ContextIntegrationTest {
