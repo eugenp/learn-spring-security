53986b1c858247e54ea71cd21731a6870e347b49
musibs <musib.somnath@gmail.com>
2021-05-09 20:55:33 +0530

    Spring Boot version upgrade m1-lesson7

--- m1-lesson7/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ m1-lesson7/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -1,10 +1,13 @@
 package com.baeldung.lss.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -18,8 +21,8 @@ public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception { // @formatter:off 
         auth.
-            inMemoryAuthentication().
-            withUser("user").password("pass").
+            inMemoryAuthentication().passwordEncoder(passwordEncoder()).
+            withUser("user").password(passwordEncoder().encode("pass")).
             roles("USER");
     } // @formatter:on
 
@@ -42,4 +45,9 @@ public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
         ;
     }
 
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+    	return new BCryptPasswordEncoder();
+    }
 }
