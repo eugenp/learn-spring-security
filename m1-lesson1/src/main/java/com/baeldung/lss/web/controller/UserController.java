35bbc1a99c262ffdc4a075eb5d24fa8312cfef7a
rcardin <riccardo.cardin@xtn-lab.com>
2021-05-12 10:57:42 +0200

    Added to module m1-lesson1 the dependency from java.validation, which is not included in spring-boot-starter-web from version 2.3.0

--- m1-lesson1/src/main/java/com/baeldung/lss/web/controller/UserController.java
+++ m1-lesson1/src/main/java/com/baeldung/lss/web/controller/UserController.java
@@ -2,6 +2,7 @@ package com.baeldung.lss.web.controller;
 
 import com.baeldung.lss.persistence.UserRepository;
 import com.baeldung.lss.web.model.User;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -11,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/")
