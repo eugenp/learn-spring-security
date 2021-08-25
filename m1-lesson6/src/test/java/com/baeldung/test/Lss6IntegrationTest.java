49593d6d7c3ca68948a30cfa7f9085cd1b604021
Przemysław Żochowski <przemyslaw.zochowski@stepwise.pl>
2021-08-25 08:08:33 +0200

    Fixed imports in the test package. The test runs and passess. Application should start now

--- m1-lesson6/src/test/java/com/baeldung/test/Lss6IntegrationTest.java
+++ m1-lesson6/src/test/java/com/baeldung/test/Lss6IntegrationTest.java
@@ -1,14 +1,12 @@
 package com.baeldung.test;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.baeldung.lss.spring.LssApp6;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = LssApp6.class, webEnvironment = WebEnvironment.RANDOM_PORT)
 public class Lss6IntegrationTest {
 
