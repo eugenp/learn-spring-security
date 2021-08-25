11c1221b16589836c356a11dc82ecf5c07727c2d
Przemysław Żochowski <przemyslaw.zochowski@stepwise.pl>
2021-08-25 13:29:57 +0200

    Added password encoder delegator bean and encrypted password with noop

--- m1-lesson6/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ m1-lesson6/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -1,13 +1,22 @@
 package com.baeldung.lss.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     public LssSecurityConfig() {
         super();
@@ -18,9 +27,9 @@ public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception { // @formatter:off 
         auth.
-            inMemoryAuthentication().
-            withUser("user").password("pass").
-            roles("USER");
+                inMemoryAuthentication().
+                withUser("user").password("{noop}pass").
+                roles("USER");
     } // @formatter:on
 
     @Override
