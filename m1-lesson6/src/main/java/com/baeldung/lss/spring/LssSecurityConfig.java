6f56656f69c81aba6d4f8eb3ddf57b990c6e79da
Przemysław Żochowski <przemyslaw.zochowski@stepwise.pl>
2021-08-26 07:47:53 +0200

    Pointing specifically to Bcrypt

--- m1-lesson6/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ m1-lesson6/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
@@ -15,7 +15,7 @@ public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 
     public LssSecurityConfig() {
