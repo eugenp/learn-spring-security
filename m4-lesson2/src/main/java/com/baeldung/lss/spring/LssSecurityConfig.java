444fc6a17a25b2265e65d61ce718760f6b4bc35d
Ger Roza <rozagerardo@gmail.com>
2022-03-17 16:55:48 -0300

    Upgrade to boot 2.6.2

--- m4-lesson2/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ m4-lesson2/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -1,21 +1,22 @@
 package com.baeldung.lss.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    public LssSecurityConfig() {
+    private PasswordEncoder passwordEncoder;
+
+    public LssSecurityConfig(PasswordEncoder passwordEncoder) {
         super();
+        this.passwordEncoder = passwordEncoder;
     }
 
     //
@@ -23,8 +24,8 @@ public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {// @formatter:off
         auth.
-            inMemoryAuthentication().passwordEncoder(passwordEncoder()).
-            withUser("user").password(passwordEncoder().encode("pass")).roles("USER");
+            inMemoryAuthentication().passwordEncoder(passwordEncoder).
+            withUser("user").password(passwordEncoder.encode("pass")).roles("USER");
     } // @formatter:on
 
     @Override
@@ -47,10 +48,5 @@ public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
         .csrf().disable()
         ;
     } // @formatter:on
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
 }
