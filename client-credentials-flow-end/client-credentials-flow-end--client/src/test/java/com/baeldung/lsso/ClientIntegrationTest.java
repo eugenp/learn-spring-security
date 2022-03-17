3280b92b19282c65d19b24a0416240d12dcb6a75
Ger Roza <rozagerardo@gmail.com>
2022-03-17 17:23:08 -0300

    Add Integration Tests to modules with different setup/functionality

---
+++ client-credentials-flow-end/client-credentials-flow-end--client/src/test/java/com/baeldung/lsso/ClientIntegrationTest.java
@@ -0,0 +1,111 @@
+package com.baeldung.lsso;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URI;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.assertj.core.api.Condition;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ClientIntegrationTest {
+
+    @Value("${resourceserver.api.project.url:http://localhost:8081/lsso-resource-server/api/projects/}")
+    private String projectsEndpoint;
+
+    @Value("${spring.security.oauth2.client.provider.customClientCredentials.token-uri}")
+    private String configuredTokenUri;
+
+    @Value("${spring.security.oauth2.client.registration.customClientCredentials.scope}")
+    private String configuredScope;
+
+    private static final String ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9." +
+            "eyJzdWIiOiJ0ZXN0LWNsaWVudCIsIm5hbWUiOiJiYWVsZHVuZyBjbGllbnQiLCJzY29wZSI6IndyaXRlIHJlYWQiLCJpYXQiOjE1MTYyMzkwMjJ9." +
+            "wZnm_kcu2lZVB3s3OUbz7AKWvLRyiPVlLP-asZlOvt0";
+
+    private static MockWebServer authServer;
+
+    private static MockWebServer resourceServer;
+
+    @BeforeAll
+    public static void setUp() throws Exception {
+    	LoggerListAppender.clearEventList();
+
+        authServer = new MockWebServer();
+        resourceServer = new MockWebServer();
+
+        String mockedAccessToken = "{" +
+        "  \"access_token\": \"" + ACCESS_TOKEN + "\"," +
+        "  \"token_type\": \"bearer\"," +
+        "  \"expires_in\": 3600" +
+        "}";
+
+        authServer.enqueue(new MockResponse().setBody(mockedAccessToken)
+                .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE));
+
+        String mockedResources = "[{\"id\":1,\"name\":\"Project 1\",\"dateCreated\":\"2019-06-13\"},{\"id\":2,\"name\":\"Project 2\",\"dateCreated\":\"2019-06-14\"},{\"id\":3,\"name\":\"Project 3\",\"dateCreated\":\"2019-06-15\"}]";
+
+        resourceServer.enqueue(new MockResponse().setBody(mockedResources)
+                .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE));
+
+        authServer.start(8083);
+        resourceServer.start(8081);
+    }
+
+    @AfterAll
+    public static void tearDown() throws Exception {
+        authServer.shutdown();
+        resourceServer.shutdown();
+    }
+
+    @Test
+    public void givenAuthServerAndResourceServer_whenRequestResourceWithClientCredentials_thenProcessExecutesOk() throws Exception {
+        // ensure the token request is correct
+        RecordedRequest tokenRequest = authServer.takeRequest();
+        assertThat(tokenRequest.getMethod()).isEqualTo(HttpMethod.POST.name());
+        // ensure the tokenUri being requested is correct
+        String tokenEndpointPath = new URI(configuredTokenUri).getPath();
+        assertThat(tokenRequest.getPath()).isEqualTo(tokenEndpointPath);
+
+        // ensure the token request contains the correct body
+        String requestBody = URLDecoder.decode(tokenRequest.getBody()
+                .readUtf8(), StandardCharsets.UTF_8.name());
+        Map<String, String> mappedBody = Arrays.stream(requestBody.split("&"))
+                .collect(Collectors.toMap(param -> param.split("=")[0], param -> param.split("=")[1]));
+        assertThat(mappedBody).containsEntry("grant_type", "client_credentials");
+        assertThat(mappedBody).containsEntry("scope", configuredScope.replace(",", ""));
+
+        // validate initial resource request after client gets authentication
+        RecordedRequest authenticatedResourceRequest = resourceServer.takeRequest();
+        assertThat(authenticatedResourceRequest.getMethod()).isEqualTo(HttpMethod.GET.name());
+        String projectsPath = new URI(projectsEndpoint).getPath();
+        assertThat(authenticatedResourceRequest.getPath()).isEqualTo(projectsPath);
+        assertThat(authenticatedResourceRequest.getHeader(HttpHeaders.AUTHORIZATION)).isEqualTo("Bearer " + ACCESS_TOKEN);
+        
+        Thread.sleep(1000); // wait for scheduled to log after receiving responses
+
+        assertThat(LoggerListAppender.getEvents()).haveAtLeastOne(eventContains("Projects in the repository:"));
+    }
+
+    private Condition<ILoggingEvent> eventContains(String substring) {
+        return new Condition<ILoggingEvent>(entry -> (substring == null || (entry.getFormattedMessage() != null && entry.getFormattedMessage()
+                .contains(substring))), String.format("entry with message '%s'", substring));
+    }
+}
---
+++ oauth2-and-openid-connect-end/oauth2-and-openid-connect-end--client/src/test/java/com/baeldung/lsso/Oauth2ClientIntegrationTest.java
@@ -0,0 +1,277 @@
+package com.baeldung.lsso;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.endsWith;
+import static org.hamcrest.CoreMatchers.startsWith;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.ExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jose.crypto.RSASSASigner;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.gen.RSAKeyGenerator;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+public class Oauth2ClientIntegrationTest {
+
+    private final String CLIENT_SECURED_URL = "/projects";
+    MockWebServer authServer;
+    MockWebServer resourceServer;
+    @Autowired
+    ObjectMapper objectMapper;
+    private String REDIRECT_URI = "/login/oauth2/code/custom?state=%s&code=%s";
+    @Value("${spring.security.oauth2.client.provider.custom.authorization-uri}")
+    private String authServerAuthorizationURL;
+    @Value("${spring.security.oauth2.client.registration.custom.redirect-uri}")
+    private String configuredRedirectUri;
+    @Value("${spring.security.oauth2.client.provider.custom.token-uri}")
+    private String configuredTokenUri;
+    @Value("${spring.security.oauth2.client.provider.custom.user-info-uri}")
+    private String configuredUserInfoUri;
+    @Value("${spring.security.oauth2.client.provider.custom.jwk-set-uri}")
+    private String jwkSetURI;
+    @Value("${resourceserver.api.project.url}")
+    private String projectsUrl;
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        createAuthServer();
+
+        createResourceServer();
+
+        webTestClient = webTestClient.mutate()
+            .responseTimeout(Duration.ofMillis(300000))
+            .build();
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        authServer.shutdown();
+        resourceServer.shutdown();
+    }
+
+    private void createResourceServer() throws IOException {
+        resourceServer = new MockWebServer();
+        resourceServer.start(8081);
+    }
+
+    private void createAuthServer() throws IOException {
+        authServer = new MockWebServer();
+        authServer.start(8083);
+    }
+
+    @Test
+    public void givenAuthServerAndResourceServer_whenPerformClientLoginProcess_thenProcessExecutesOk() throws Exception {
+        // mimic login button action
+        ExchangeResult result = this.webTestClient.get()
+            .uri(CLIENT_SECURED_URL)
+            .exchange()
+            .expectStatus()
+            .isFound()
+            .expectHeader()
+            .value(HttpHeaders.LOCATION, endsWith("/oauth2/authorization/custom"))
+            .returnResult(Void.class);
+
+        // redirects to 'custom' OAuth authorization endpoint
+        String cookieSession = result.getResponseCookies()
+            .getFirst("JSESSIONID")
+            .getValue();
+        String redirectTarget = result.getResponseHeaders()
+            .getFirst(HttpHeaders.LOCATION);
+
+        result = this.webTestClient.get()
+            .uri(redirectTarget)
+            .cookie("JSESSIONID", cookieSession)
+            .exchange()
+            .expectStatus()
+            .isFound()
+            .expectHeader()
+            .value(HttpHeaders.LOCATION, startsWith(authServerAuthorizationURL))
+            .returnResult(Void.class);
+
+        // request to authorization endpoint contains state attribute
+        String authorizationURL = result.getResponseHeaders()
+            .getFirst(HttpHeaders.LOCATION);
+        String state = URLDecoder.decode(authorizationURL.split("state=")[1].split("&")[0], StandardCharsets.UTF_8.toString());
+        String nonce = URLDecoder.decode(authorizationURL.split("nonce=")[1].split("&")[0], StandardCharsets.UTF_8.toString());
+
+        // Initialise a RSAKey Generator
+        // To be used for signing the mock token
+        RSAKey rsaJWK = new RSAKeyGenerator(2048).keyID("123")
+            .generate();
+
+        // prepare token endpoint response
+        String idToken = mockIdToken(nonce, rsaJWK);
+        String accessToken = "abc987";
+        String mockedAccessToken = mockAccessToken(idToken, accessToken);
+        authServer.enqueue(new MockResponse().setBody(mockedAccessToken)
+            .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE));
+
+        // prepare JWKSet Endpoint response exposing public keys
+        String mockJWKResponse = mockJwkSet(rsaJWK);
+        authServer.enqueue(new MockResponse().setBody(mockJWKResponse)
+            .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE));
+
+        // prepare UserInfo mocked response
+        String mockedUserInfo = mockUserInfo();
+        authServer.enqueue(new MockResponse().setBody(mockedUserInfo)
+            .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE));
+
+        // send request to redirect_uri with code and state
+        String code = "123";
+        result = this.webTestClient.get()
+            .uri(String.format(REDIRECT_URI, state, code))
+            .cookie("JSESSIONID", cookieSession)
+            .exchange()
+            .expectStatus()
+            .isFound()
+            .expectHeader()
+            .value(HttpHeaders.LOCATION, endsWith(CLIENT_SECURED_URL))
+            .returnResult(Void.class);
+
+        // assert that Access Token Endpoint was requested as expected
+        RecordedRequest capturedTokenRequest = authServer.takeRequest();
+        assertThat(capturedTokenRequest.getMethod()).isEqualTo(HttpMethod.POST.name());
+        String tokenEndpointPath = new URI(configuredTokenUri).getPath();
+        assertThat(capturedTokenRequest.getPath()).isEqualTo(tokenEndpointPath);
+        String requestBody = URLDecoder.decode(capturedTokenRequest.getBody()
+            .readUtf8(), StandardCharsets.UTF_8.name());
+        Map<String, String> mappedBody = Arrays.stream(requestBody.split("&"))
+            .collect(Collectors.toMap(param -> param.split("=")[0], param -> param.split("=")[1]));
+        assertThat(mappedBody).containsEntry("grant_type", "authorization_code");
+        assertThat(mappedBody).containsEntry("code", code);
+        assertThat(mappedBody).containsEntry("redirect_uri", configuredRedirectUri);
+
+        // assert that jwkSet endpoint was requested as expected
+        RecordedRequest capturedJWKSetRequest = authServer.takeRequest();
+        assertThat(capturedJWKSetRequest.getMethod()).isEqualTo(HttpMethod.GET.name());
+        String jwkSetPath = new URI(jwkSetURI).getPath();
+        assertThat(capturedJWKSetRequest.getPath()).isEqualTo(jwkSetPath);
+
+        // assert that userInfo endpoint was requested as expected
+        RecordedRequest capturedUserInfoRequest = authServer.takeRequest();
+        assertThat(capturedUserInfoRequest.getMethod()).isEqualTo(HttpMethod.GET.name());
+        String userInfoPath = new URI(configuredUserInfoUri).getPath();
+        assertThat(capturedUserInfoRequest.getPath()).isEqualTo(userInfoPath);
+        assertThat(capturedUserInfoRequest.getHeader(HttpHeaders.AUTHORIZATION)).isEqualTo("Bearer " + accessToken);
+
+        String mockedResources = "[{\"id\":1,\"name\":\"Project 1\",\"dateCreated\":\"2019-06-13\"},{\"id\":2,\"name\":\"Project 2\",\"dateCreated\":\"2019-06-14\"},{\"id\":3,\"name\":\"Project 3\",\"dateCreated\":\"2019-06-15\"}]";
+
+        resourceServer.enqueue(new MockResponse().setBody(mockedResources)
+            .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE));
+
+        // now we're redirected back to the /projects endpoint
+        // when accessing it, Client should send Access Token as Bearer token in header
+        String newCookieSession = result.getResponseCookies()
+            .getFirst("JSESSIONID")
+            .getValue();
+
+        this.webTestClient.get()
+            .uri(CLIENT_SECURED_URL)
+            .cookie("JSESSIONID", newCookieSession)
+            .exchange()
+            .expectStatus()
+            .isOk()
+            .expectBody()
+            .consumeWith(response -> {
+                String bodyAsString = new String(response.getResponseBodyContent());
+                assertThat(bodyAsString).contains("Project 1")
+                    .contains("Project 2")
+                    .contains("Project 3")
+                    .doesNotContain("Project 4");
+            });
+
+        RecordedRequest capturedProjectRequest = resourceServer.takeRequest();
+        assertThat(capturedProjectRequest.getMethod()).isEqualTo(HttpMethod.GET.name());
+        String projectsPath = new URI(projectsUrl).getPath();
+        assertThat(capturedProjectRequest.getPath()).isEqualTo(projectsPath);
+        assertThat(capturedProjectRequest.getHeader(HttpHeaders.AUTHORIZATION)).isEqualTo("Bearer " + accessToken);
+    }
+
+    private String mockIdToken(String nonce, RSAKey rsaJWK) throws JOSEException {
+        JWSSigner signer = new RSASSASigner(rsaJWK);
+        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder().subject("fakeSub")
+            .issuer("http://localhost/mock.com")
+            .audience("lssoClient")
+            .issueTime(new Date())
+            .expirationTime(new Date(new Date().getTime() + 600 * 1000))
+            .claim("nonce", nonce)
+            .claim("preferred_username", "theUsername")
+            .build();
+        SignedJWT signedJWT = new SignedJWT(new JWSHeader.Builder(JWSAlgorithm.RS256).keyID(rsaJWK.getKeyID())
+            .build(), claimsSet);
+        signedJWT.sign(signer);
+        return signedJWT.serialize();
+    }
+
+    private String mockAccessToken(String idToken, String tokenValue) {
+        //@formatter:off
+        return "{" +
+            "  \"access_token\": \"" + tokenValue + "\"," +
+            "  \"token_type\": \"bearer\"," +
+            "  \"expires_in\": 3600," +
+            "  \"scope\": \"email\"," +
+            "  \"id_token\": \"" + idToken + "\"" +
+            "}";
+    }
+    
+    private String mockJwkSet(RSAKey rsaJWK) throws JsonProcessingException {
+        JWKSet jwk = new JWKSet(rsaJWK);
+        return objectMapper.writeValueAsString(jwk.toJSONObject());
+    }
+
+
+    private String mockUserInfo() {
+        //@formatter:off
+        return "{" +
+            "  \"preferred_username\": \"theUsername\"," +
+            "  \"sub\": \"fakeSub\"" +
+            "}";
+    }
+    
+    @Test
+    public void whenUnauthorized_thenRedirect() throws Exception {
+        this.webTestClient.get()
+            .uri(CLIENT_SECURED_URL)
+            .exchange()
+            .expectStatus()
+            .is3xxRedirection();
+    }
+}
