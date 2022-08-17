3553516924aab1fdcebb8d567e4d842fae2dd432
Daniel Runvik <daniel.b.walther@gmail.com>
2022-08-17 16:12:05 +0200

    Handle warnings in logs

--- m13-lesson4-start/src/main/java/com/baeldung/lss/spring/LssApp5.java
+++ m13-lesson4-start/src/main/java/com/baeldung/lss/spring/LssApp5.java
@@ -3,10 +3,12 @@ package com.baeldung.lss.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
 @ComponentScan("com.baeldung.lss")
 @EnableJpaRepositories("com.baeldung.lss")
 @EntityScan("com.baeldung.lss.web.model")
