e2bb4d040856671834bbf9d391a0a19353e78591
Daniel Runvik <daniel.b.walther@gmail.com>
2022-09-30 10:26:50 +0200

    Remove deprecated WebSecurityConfigurerAdapter

--- oauth2-social-login-start/oauth2-social-login-start--client/src/main/java/com/baeldung/lsso/spring/ClientSecurityConfig.java
+++ oauth2-social-login-start/oauth2-social-login-start--client/src/main/java/com/baeldung/lsso/spring/ClientSecurityConfig.java
@@ -1,21 +1,23 @@
 package com.baeldung.lsso.spring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @EnableWebSecurity
-public class ClientSecurityConfig extends WebSecurityConfigurerAdapter {
+public class ClientSecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {// @formatter:off
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {// @formatter:off
         http.authorizeRequests()
             .antMatchers("/").permitAll()
             .anyRequest().authenticated();
+        return http.build();
     }// @formatter:on
 
     //@Bean
