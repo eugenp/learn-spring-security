8c81ab77c47dc259ff8f5d666425e2e52491f6ea
Daniel Runvik <daniel.b.walther@gmail.com>
2022-09-30 12:29:05 +0200

    Upgrade to Spring Boot 2.7.3
    Also renamed user table and removed deprecated WebSecurityConfigurerAdapter

--- setting-up-users-at-startup-end/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ setting-up-users-at-startup-end/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,11 +14,12 @@ import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 @EnableWebSecurity
 @Configuration
-public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
+public class LssSecurityConfig {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -33,11 +35,11 @@ public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService)
-            .passwordEncoder(passwordEncoder);
+                .passwordEncoder(passwordEncoder);
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {// @formatter:off
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {// @formatter:off
         http
         .authorizeRequests()
                 .antMatchers("/badUser*","/js/**").permitAll()
@@ -54,8 +56,8 @@ public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
         .and().sessionManagement().maximumSessions(1).sessionRegistry(sessionRegistry()).and().sessionFixation().none()
 
         .and()
-        .csrf().disable()
-        ;
+        .csrf().disable();
+        return http.build();
     } // @formatter:on
 
     @Bean
