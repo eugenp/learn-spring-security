91aeda542ab82a328155ad542cb1c00d75e704bd
Daniel Runvik <daniel.b.walther@gmail.com>
2022-10-19 19:19:21 +0200

    Remove deprecated WebSecurityConfigurerAdapter

--- m5-on-method-auth-expressions-end/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ m5-on-method-auth-expressions-end/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -1,18 +1,19 @@
 package com.baeldung.lss.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
+public class LssSecurityConfig {
 
     private PasswordEncoder passwordEncoder;
 
@@ -32,8 +33,8 @@ public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
             ;
     } // @formatter:on
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception { // @formatter:off
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {// @formatter:off
         http
         .authorizeRequests()
             
@@ -50,8 +51,8 @@ public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
         .logout().permitAll().logoutUrl("/logout")
         
         .and()
-        .csrf().disable()
-        ;
+        .csrf().disable();
+        return http.build();
     }
 
 }
