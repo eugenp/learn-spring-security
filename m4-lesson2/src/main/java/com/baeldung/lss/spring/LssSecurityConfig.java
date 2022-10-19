cf9b65005755b947da2fd98de6729087843af081
Daniel Runvik <daniel.b.walther@gmail.com>
2022-10-19 19:18:09 +0200

    Remove deprecated WebSecurityConfigurerAdapter

--- m4-lesson2/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ m4-lesson2/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -1,16 +1,17 @@
 package com.baeldung.lss.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
-public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
+public class LssSecurityConfig {
 
     private PasswordEncoder passwordEncoder;
 
@@ -28,8 +29,8 @@ public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
             withUser("user").password(passwordEncoder.encode("pass")).roles("USER");
     } // @formatter:on
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {// @formatter:off
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {// @formatter:off
         http
         .authorizeRequests()
                 .antMatchers("/signup", "/user/register").permitAll()
@@ -45,8 +46,8 @@ public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
         .logout().permitAll().logoutUrl("/logout")
 
         .and()
-        .csrf().disable()
-        ;
+        .csrf().disable();
+        return http.build();
     } // @formatter:on
 
 }
