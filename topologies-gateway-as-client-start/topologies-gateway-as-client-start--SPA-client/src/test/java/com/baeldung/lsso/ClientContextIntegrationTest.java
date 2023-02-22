f9a094a857d0d37a926e9e6a3c43220f2532c006
Ger Roza <rozagerardo@gmail.com>
2023-02-22 16:24:11 -0300

    cleaned up unused imports

--- topologies-gateway-as-client-start/topologies-gateway-as-client-start--SPA-client/src/test/java/com/baeldung/lsso/ClientContextIntegrationTest.java
+++ topologies-gateway-as-client-start/topologies-gateway-as-client-start--SPA-client/src/test/java/com/baeldung/lsso/ClientContextIntegrationTest.java
@@ -2,7 +2,6 @@ package com.baeldung.lsso;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest(classes = { LssoSpaClientApplication.class })
 public class ClientContextIntegrationTest {
