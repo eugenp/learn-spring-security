53f00ea9e0f48dea0f4a469d85b48f1b1e03df08
Ger Roza <rozagerardo@gmail.com>
2023-04-14 12:43:54 -0300

    Implemented DTOs as java records

--- legacy-stack-authorization-server-start/legacy-stack-authorization-server-start--resource-server/src/main/java/com/baeldung/lsso/web/dto/ProjectDto.java
+++ legacy-stack-authorization-server-start/legacy-stack-authorization-server-start--resource-server/src/main/java/com/baeldung/lsso/web/dto/ProjectDto.java
@@ -5,45 +5,12 @@ import java.time.LocalDate;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 
-public class ProjectDto {
+public record ProjectDto (
 
-    private Long id;
+    Long id,
 
-    private String name;
+    String name,
 
     @DateTimeFormat(iso = ISO.DATE)
-    private LocalDate dateCreated;
-
-    public ProjectDto() {
-    }
-
-    public ProjectDto(Long id, String name, LocalDate dateCreated) {
-        this.id = id;
-        this.name = name;
-        this.dateCreated = dateCreated;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getDateCreated() {
-        return dateCreated;
-    }
-
-    public void setDateCreated(LocalDate dateCreated) {
-        this.dateCreated = dateCreated;
-    }
+    LocalDate dateCreated) {
 }
