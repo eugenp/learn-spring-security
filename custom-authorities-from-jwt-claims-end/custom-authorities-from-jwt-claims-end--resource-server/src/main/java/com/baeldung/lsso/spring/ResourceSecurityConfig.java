3910d71e69e2312d8ba2ea86934ea1b3e9f9b361
Ger Roza <rozagerardo@gmail.com>
2023-04-14 12:41:20 -0300

    Implemented DTOs as java records

--- custom-authorities-from-jwt-claims-end/custom-authorities-from-jwt-claims-end--resource-server/src/main/java/com/baeldung/lsso/spring/ResourceSecurityConfig.java
+++ custom-authorities-from-jwt-claims-end/custom-authorities-from-jwt-claims-end--resource-server/src/main/java/com/baeldung/lsso/spring/ResourceSecurityConfig.java
@@ -2,11 +2,8 @@ package com.baeldung.lsso.spring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -31,7 +28,7 @@ public class ResourceSecurityConfig {
     }//@formatter:on
 
     @Bean
-    Converter<Jwt, AbstractAuthenticationToken> customJwtAuthenticationConverter() {
+    JwtAuthenticationConverter customJwtAuthenticationConverter() {
         JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new CustomAuthoritiesExtractor());
         return jwtAuthenticationConverter;
