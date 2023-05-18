7e5a5703498a4909a6ad00bec24c203d1c7edc7e
Ger Roza <rozagerardo@gmail.com>
2023-05-18 11:40:48 -0300

    Removed annotation without effect

--- basic-authorization-end/basic-authorization-end--resource-server/src/main/java/com/baeldung/lsso/web/dto/ProjectDto.java
+++ basic-authorization-end/basic-authorization-end--resource-server/src/main/java/com/baeldung/lsso/web/dto/ProjectDto.java
@@ -2,15 +2,11 @@ package com.baeldung.lsso.web.dto;
 
 import java.time.LocalDate;
 
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.format.annotation.DateTimeFormat.ISO;
-
 public record ProjectDto (
 
     Long id,
 
     String name,
 
-    @DateTimeFormat(iso = ISO.DATE)
     LocalDate dateCreated) {
 }
