860bdc4af55b30fb22073a49e41de0de718a96c7
Ger Roza <rozagerardo@gmail.com>
2023-09-15 11:44:08 -0300

    Upgraded to Boot 3.1

--- oauth2-social-login-end/oauth2-social-login-end--client/src/main/java/com/baeldung/lsso/spring/ClientSecurityConfig.java
+++ oauth2-social-login-end/oauth2-social-login-end--client/src/main/java/com/baeldung/lsso/spring/ClientSecurityConfig.java
@@ -2,6 +2,7 @@ package com.baeldung.lsso.spring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
@@ -17,9 +18,8 @@ public class ClientSecurityConfig {
         http.authorizeHttpRequests(authorize -> authorize
 	            .requestMatchers("/").permitAll()
 	            .anyRequest().authenticated())
-            .oauth2Login()
-            .and()
-            .logout().logoutSuccessUrl("/");
+            .oauth2Login(Customizer.withDefaults())
+            .logout(logoutCustomizer -> logoutCustomizer.logoutSuccessUrl("/"));
         return http.build();
     }// @formatter:on
 
