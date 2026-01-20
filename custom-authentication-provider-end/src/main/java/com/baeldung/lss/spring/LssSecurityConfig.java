3407af0d8e92a68cfc9770aad80a7e7bf2d11918
Baeldung CI User <baeldungci@baeldung.com>
2026-01-20 02:13:40 +0000

    Merge pull request #1379 from timis1/JAVA-50478
    JAVA-50478 Update LSSC M8 L2 to text-based lesson

---
+++ custom-authentication-provider-end/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -0,0 +1,67 @@
+package com.baeldung.lss.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.baeldung.lss.model.User;
+import com.baeldung.lss.persistence.UserRepository;
+import com.baeldung.lss.security.CustomAuthenticationProvider;
+
+import jakarta.annotation.PostConstruct;
+
+@EnableWebSecurity
+@Configuration
+public class LssSecurityConfig {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private CustomAuthenticationProvider customAuthenticationProvider;
+
+    private PasswordEncoder passwordEncoder;
+
+    public LssSecurityConfig(PasswordEncoder passwordEncoder) {
+        super();
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    //
+
+    @PostConstruct
+    private void saveTestUser() {
+        final User user = new User();
+        user.setEmail("test@email.com");
+        user.setPassword(passwordEncoder.encode("pass"));
+        userRepository.save(user);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {// @formatter:off
+        auth.authenticationProvider(customAuthenticationProvider);
+    } // @formatter:on
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {// @formatter:off
+        http
+        .authorizeHttpRequests((authorize) -> authorize
+                .requestMatchers("/badUser*","/js/**").permitAll()
+                .anyRequest().authenticated())
+
+        .formLogin((form) -> form
+                .loginPage("/login").permitAll()
+                .loginProcessingUrl("/doLogin"))
+
+        .logout((logout) -> logout
+                .permitAll().logoutUrl("/logout"))
+
+        .csrf((csrf) -> csrf.disable());
+        return http.build();
+    } // @formatter:on
+}
---
+++ multiple-providers-and-authentication-manager-end/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -0,0 +1,83 @@
+package com.baeldung.lss.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.baeldung.lss.model.User;
+import com.baeldung.lss.persistence.UserRepository;
+import com.baeldung.lss.security.CustomAuthenticationProvider;
+import com.google.common.collect.Lists;
+
+import jakarta.annotation.PostConstruct;
+
+@EnableWebSecurity
+@Configuration
+public class LssSecurityConfig {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private CustomAuthenticationProvider customAuthenticationProvider;
+
+    private PasswordEncoder passwordEncoder;
+
+    public LssSecurityConfig(PasswordEncoder passwordEncoder) {
+        super();
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    //
+
+    @PostConstruct
+    private void saveTestUser() {
+        final User user = new User();
+        user.setEmail("test@email.com");
+        user.setPassword(passwordEncoder.encode("pass"));
+        userRepository.save(user);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        /*final DaoAuthenticationProvider daoAuthProvider = new DaoAuthenticationProvider();
+        daoAuthProvider.setUserDetailsService(userDetailsService);
+        auth.authenticationProvider(customAuthenticationProvider).authenticationProvider(daoAuthProvider);*/
+
+        // auth.parentAuthenticationManager(new ProviderManager(Lists.newArrayList(customAuthenticationProvider)));
+
+        ProviderManager authenticationManager = new ProviderManager(Lists.newArrayList(customAuthenticationProvider));
+        authenticationManager.setEraseCredentialsAfterAuthentication(false);
+        auth.parentAuthenticationManager(authenticationManager);
+        // auth.eraseCredentials(false).userDetailsService(userDetailsService);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {// @formatter:off
+        http
+        .authorizeHttpRequests((authorize) -> authorize
+                .requestMatchers("/badUser*","/js/**").permitAll()
+                .anyRequest().authenticated())
+
+        .formLogin((form) -> form
+                .loginPage("/login").permitAll()
+                .loginProcessingUrl("/doLogin"))
+
+        .logout((logout) -> logout
+                .permitAll().logoutUrl("/logout"))
+
+        .csrf((csrf) -> csrf.disable());
+        return http.build();
+    } // @formatter:on
+}
